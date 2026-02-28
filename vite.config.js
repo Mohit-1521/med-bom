0fccf261bc6a3230383d8e86da4a00ac23b31250
Mohit Sancheti <mohit.sancheti@flairlabs.com>
2026-03-01 04:04:12 +0530

    chore: Remove `allowedHosts` from Vite server configuration.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ["77f2-203-115-70-179.ngrok-free.app"]
-  }
 })
